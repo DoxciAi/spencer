05ec92b862e7edf16cb0a27680f042f4dfa455a1
RobertoFORTs <bobfortesneto@gmail.com>
2024-05-06 18:08:52 -0400

    feat: add some usefull endpoints for tracker usage

--- utils/managementApi/trackers.js
+++ utils/managementApi/trackers.js
@@ -18,4 +18,49 @@ export class TrackerAPI {
     const data = await response.json();
     return data;
   }
+
+  async getTrackers() {
+    const options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        'x-api-key': this.accesstoken
+      }
+    };
+
+    const response = await fetch('https://api.symbl.ai/v1/manage/trackers', options);
+    const data = await response.json();
+    return data;
+  }
+
+  async getTrackerById (trackerId) {
+    const options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        'x-api-key': this.accesstoken
+      }
+    };
+
+    const response = await fetch(`https://api.symbl.ai/v1/manage/trackers/${trackerId}`, options);
+    const data = await response.json();
+    return data;
+  }
+
+  async updateTracker(trackerId, trackerUpdateObject) {
+    const options = {
+      method: 'PATCH',
+      headers: {
+        accept: 'application/json',
+        'x-api-key': this.accesstoken,
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(trackerUpdateObject)
+    };
+
+    const response = await fetch(`https://api.symbl.ai/v1/manage/trackers/${trackerId}`, options);
+    const data = await response.json();
+    return data;
+  }
+
 }
